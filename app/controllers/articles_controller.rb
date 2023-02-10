adaa1e6d946724871ee09a69cf7a2b93d8d799fc
GOD-oda <last.pass.oda@gmail.com>
2023-02-10 18:56:45 +0900

    Add create form to index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article, notice: "Article was successfully created."
+      render :create
     else
       render :new, status: :unprocessable_entity
     end
